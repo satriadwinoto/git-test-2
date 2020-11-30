0e716d7d710a75c52e79111f1279d10db04f2795
Satria Dwinoto <satriadwinoto@gmail.com>
2020-11-30 16:52:08 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In suscipit adipisci modi labore ipsam, magni nostrum provident tenetur natus amet? Eaque harum eligendi provident nisi culpa dolores dicta. Harum, adipisci?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
